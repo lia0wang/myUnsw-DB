002efefd1c9e628bce228e59de2ff2ade0ad00bf
WANG LIAO <z5306312@ad.unsw.edu.au>
2021-11-15 03:45:07 +1100

    Done q2

--- helpers.py
+++ helpers.py
@@ -87,7 +87,7 @@ def getUocAndWam(uoc_lst, def_uoc_lst, wam_lst):
 
     return[uoc_sum, sum(wam_sum), def_uoc_sum, wam_average]
 
-def getOrgunits(db,pg_id):
+def getOrgunitsProgram(db,pg_id):
   cur = db.cursor()
   qry = """
   select o.longname
@@ -96,6 +96,22 @@ def getOrgunits(db,pg_id):
   where  p.id = %s
   """
   cur.execute(qry,[pg_id])
+  info = cur.fetchone()
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def getOrgunitsStream(db,st_id):
+  cur = db.cursor()
+  qry = """
+  select o.longname
+  from   Streams s
+         join Orgunits o on s.offeredby = o.id
+  where  s.id = %s
+  """
+  cur.execute(qry,[st_id])
   info = cur.fetchone()
   cur.close()
   if not info:
@@ -114,6 +130,25 @@ def getProgramDetails(db,pg_id):
   where p.id = %s
   """
   cur.execute(qry,[pg_id])
+  info = cur.fetchall()
+
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def getStreamDetails(db,st_id):
+  cur = db.cursor()
+  qry = """
+  select aog.name, aog.definition, r.name, r.min_req, r.max_req
+  from streams s
+          join stream_rules sr on s.id = sr.stream
+          join rules r on sr.rule = r.id
+          join academic_object_groups aog on aog.id = r.ao_group
+  where s.id = %s
+  """
+  cur.execute(qry,[st_id])
   info = cur.fetchall()
 
   cur.close()
@@ -172,3 +207,121 @@ def getType(db, pg_id):
     return None
   else:
     return info
+
+def getStreamType(db, st_id):
+  cur = db.cursor()
+  qry = """
+  select s.stype
+  from streams s
+  where s.name = %s
+  """
+  cur.execute(qry,[st_id])
+  info = cur.fetchone()
+
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def getStreamRulesType(db, st_id):
+  cur = db.cursor()
+  qry = """
+  select r.type
+  from streams s
+          join stream_rules sr on sr.stream = s.id
+          join rules r on r.id = sr.rule
+  where s.id = %s
+  """
+  cur.execute(qry,[st_id])
+  info = cur.fetchone()
+
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def getProgramRulesType(db, pg_id):
+  cur = db.cursor()
+  qry = """
+  select r.type
+  from programs p
+          join program_rules pr on pr.program = p.id
+          join rules r on r.id = pr.rule
+  where p.id = %s
+  """
+  cur.execute(qry,[pg_id])
+  info = cur.fetchone()
+
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def getRulesType(db, name):
+  cur = db.cursor()
+  qry = """
+  select r.type
+  from rules r
+  where r.name = %s
+  """
+  cur.execute(qry,[name])
+  info = cur.fetchone()
+
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def getAogDefby(db, name):
+  cur = db.cursor()
+  qry = """
+  select a.defby
+  from academic_object_groups a
+  where a.name = %s
+  """
+  cur.execute(qry,[name])
+  info = cur.fetchone()
+
+  cur.close()
+  if not info:
+    return None
+  else:
+    return info
+
+def minOrMax(min, max, course):
+    # min and max are null ... nothing to be displayed
+    if not min and not max:
+      pass
+    # min is not null, max is null ... "at least min"
+    elif min and not max:
+      print(f"at least {min} UOC courses from {course}")
+    # min is null, max is not null ... "up to max"
+    elif not min and max:
+      print(f"up to {max} UOC courses from {course}")
+    # both are not null and min < max ... "between min and max"
+    elif min and max and min < max:
+      print(f"between {min} and {max} UOC courses from {course}")
+    # both are not null and min = max ... "min"
+    elif min and max and min == max:
+      print(f"{min} UOC courses from {course}")
+
+def minOrMaxFree(min, max, course):
+    # min and max are null ... nothing to be displayed
+    if not min and not max:
+      pass
+    # min is not null, max is null ... "at least min"
+    elif min and not max:
+      print(f"at least {min} UOC of {course}")
+    # min is null, max is not null ... "up to max"
+    elif not min and max:
+      print(f"up to {max} UOC of {course}")
+    # both are not null and min < max ... "between min and max"
+    elif min and max and min < max:
+      print(f"between {min} and {max} UOC of {course}")
+    # both are not null and min = max ... "min"
+    elif min and max and min == max:
+      print(f"{min} UOC of {course}")
